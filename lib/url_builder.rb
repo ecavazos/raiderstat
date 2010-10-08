197c476b54c65aa77c77ab402c604187f3784503
ecavazos <ejcavazos@gmail.com>
2010-10-07 22:28:03 -0700

    refactored url builder

--- lib/url_builder.rb
+++ lib/url_builder.rb
@@ -4,32 +4,22 @@ module URLBuilder
       side = side.to_sym
       stat = stat.to_sym
 
-      type = {
-        :offense => 'offensiveStatisticCategory',
-        :defense => 'defensiveStatisticCategory'
+      si = {
+        :offense => ['offensiveStatisticCategory', 'TM', 2],
+        :defense => ['defensiveStatisticCategory', 'OPP', 1]
       }
-      role = {
-        :offense => 'TM',
-        :defense => 'OPP'
+
+      st = {
+        :total => ['GAME_STATS', 'TOTAL_YARDS_GAME_AVG'],
+        :passing => ['TEAM_PASSING', 'PASSING_NET_YARDS_GAME_AVG'],
+        :rushing => ['RUSHING', 'RUSHING_YARDS_PER_GAME_AVG']
       }
-      sort = {
-        :offense => 2,
-        :defense => 1
-      }
-      stat1 = {
-        :total => 'GAME_STATS',
-        :passing => 'TEAM_PASSING',
-        :rushing => 'RUSHING'
-      }
-      stat2 = {
-        :total   => 'TOTAL_YARDS_GAME_AVG',
-        :passing => 'PASSING_NET_YARDS_GAME_AVG',
-        :rushing => 'RUSHING_YARDS_PER_GAME_AVG'
-      }
+
+      si, st = si[side], st[stat]
 
       base = "http://www.nfl.com/stats/categorystats?tabSeq=2"
 
-      "#{base}&#{type[side]}=#{stat1[stat]}&role=#{role[side]}&d-447263-s=#{stat2[stat]}&d-447263-o=#{sort[side]}"
+      "#{base}&#{si[0]}=#{st[0]}&role=#{si[1]}&d-447263-s=#{st[1]}&d-447263-o=#{si[2]}"
     end
   end
 end
