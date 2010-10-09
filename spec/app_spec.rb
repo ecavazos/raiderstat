2c93ce6d1a58c4c9dc3fd7e7a45c179a51e9e212
ecavazos <ejcavazos@gmail.com>
2010-10-08 21:55:43 -0700

    testing view

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,16 +3,39 @@ require File.expand_path('../../app', __FILE__)
 
 App.set :environment, :test
 
-describe 'App' do
+describe "App" do
   include Rack::Test::Methods
 
   def app
     App
   end
 
-  it 'should respond to /' do
+  before(:all) do
     get '/'
+    @body = last_response.body.gsub(/\s+/, ' ')
+  end
+
+  it "should respond to /" do
     last_response.should be_ok
   end
 
+  it "should have a title of 'RaiderStat'" do
+    expected = "<title>RaiderStat</title>"
+    @body.include?(expected).should be_true
+  end
+
+  it "should have correct header" do
+    expected = "<h1>How are my Raiders this year?<\/h1>"
+    @body.include?(expected).should be_true
+  end
+
+  it "should have division rank" do
+    expected = /<dt>.+Division\sRank:.+<\/dt>.+<dd>.+\d.+<\/dd>/mx
+    last_response.body.should match(expected)
+  end
+
+  it "should have an author credit" do
+    expected = "<div id='footer'> Created by Emilio Cavazos </div>"
+    @body.include?(expected).should be_true
+  end
 end
