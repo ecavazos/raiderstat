0fe88c3b16b1a1ddb0562fac874fd0aae532dee6
ecavazos <ejcavazos@gmail.com>
2010-10-09 15:57:21 -0700

    added some config lines that help fix heroku problems

--- app.rb
+++ app.rb
@@ -12,6 +12,12 @@ require 'stat_scraper'
 class App < Sinatra::Base
 
   configure do
+    set :app_file, __FILE__
+    set :logging, Proc.new { !test? }
+    set :run, Proc.new { !test? }
+    set :dump_errors, true
+    set :haml, {:format => :html5 }
+
     DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/stats.db")
     DataMapper.auto_migrate!
   end
